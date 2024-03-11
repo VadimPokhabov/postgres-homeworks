4452067765701fe66b3558e8096a4ccbd336fb7e
Vadim <VadimPokhabov@mail.ru>
2024-03-11 06:16:37 +0300

    Написаны sql-запросы

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -29,16 +29,24 @@ CREATE TABLE orders
 	ship_city varchar(10)
 );
 
-
-SELECT * FROM customers;
-SELECT * FROM employees;
-SELECT * FROM orders;
-
-DELETE FROM customers;
-DELETE FROM employees;
-DELETE FROM orders;
-
-SELECT pg_terminate_backend(pg_stat_activity.pid)
-FROM pg_stat_activity
-WHERE pg_stat_activity.datname = 'north' -- ← изменить на свое название БД
-  AND pid <> pg_backend_pid();
+--Показать содержимое
+-- SELECT * FROM customers;
+-- SELECT * FROM employees;
+-- SELECT * FROM orders;
+--
+--Удалить содержимое
+-- DELETE FROM customers;
+-- DELETE FROM employees;
+-- DELETE FROM orders;
+--
+--Код, чтобы удалить все активные подключения к БД
+-- SELECT pg_terminate_backend(pg_stat_activity.pid)
+-- FROM pg_stat_activity
+-- WHERE pg_stat_activity.datname = 'north' -- ← изменить на свое название БД
+--   AND pid <> pg_backend_pid();
+--
+--Создание БД
+-- CREATE DATABASE test;
+--
+--Удаление БД
+-- DROP DATABASE test;
